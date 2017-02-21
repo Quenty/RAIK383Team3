ff41a29c8095540af6291ec0e48aa27d241d34cf
James Onnen <rbxquenty@gmail.com>
2017-02-21 14:15:28 -0600

    Added orders, removed order interfaces

---
+++ PracticalWerewolf/Models/Orders/OrderTrackInfo.cs
@@ -0,0 +1,19 @@
+﻿using PracticalWerewolf.Models.Trucks;
+using PracticalWerewolf.Models.UserInfos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PracticalWerewolf.Models.Orders
+{
+    public class OrderTrackInfo
+    {
+        [Key]
+        public Guid OrderTrackInfoGuid { get; set; }
+
+        Truck CurrentTruck { get; set; }
+        ContractorInfo Assignee { get; set; }
+    }
+}
