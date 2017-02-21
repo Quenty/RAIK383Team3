347352a9a8b58d28f61ae41d5f7d4338201bbb66
James Onnen <rbxquenty@gmail.com>
2017-02-21 13:13:14 -0600

    Added true identity models to the (Part of #15)

--- PracticalWerewolf/Models/IdentityModels.cs
+++ PracticalWerewolf/Models/IdentityModels.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -9,11 +10,10 @@ namespace PracticalWerewolf.Models
 {
     public class ApplicationUser : IdentityUser
     {
-
-        public virtual IContractorInfo ContractorInfo { get; set; }
-        public virtual ICustomerInfo CustomerInfo { get; set; }
-        public virtual IEmployeeInfo EmployeeInfo { get; set; }
-        public virtual IUserInfo UserInfo { get; set; }
+        public virtual ContractorInfo ContractorInfo { get; set; }
+        public virtual CustomerInfo CustomerInfo { get; set; }
+        public virtual EmployeeInfo EmployeeInfo { get; set; }
+        public virtual UserInfo UserInfo { get; set; } // 1-1
 
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
@@ -25,8 +25,37 @@ namespace PracticalWerewolf.Models
         }
     }
 
+    public class UserInfo : IUserInfo {
+        public System.Guid UserInfoGuid { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    public class CustomerInfo : ICustomerInfo
+    {
+        public System.Guid CustomerInfoGuid { get; set; }
+        public virtual IBillingInfo BillingInfo { get; set; }
+    }
+
+    public class EmployeeInfo : IEmployeeInfo
+    {
+        public System.Guid CustomerInfoGuid { get; set; }
+    }
+
+    public class ContractorInfo : IContractorInfo
+    {
+        public System.Guid ContractorInfoGuid { get; set; }
+        public virtual ITruck Truck { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        DbSet<CustomerInfo> CustomerInfo { get; set; }
+        DbSet<UserInfo> UserInfo { get; set; }
+        DbSet<EmployeeInfo> EmployeeInfo { get; set; }
+        DbSet<ContractorInfo> ContractorInfo { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
