ad4c4273b2bbe61c2db202ad82d720cd2f27c68f
ajarratt06 <ajarratt@cse.unl.edu>
2017-02-21 17:06:58 -0600

    Updated controllers
    Controllers have roles. Saving now so I can roll back if the migration
    goes terribly

--- PracticalWerewolf/Controllers/OrderController.cs
+++ PracticalWerewolf/Controllers/OrderController.cs
@@ -1,35 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace PracticalWerewolf.Controllers
 {
     public class OrderController : Controller
     {
-        // GET: Order
+        // GET: Order/Index
+        [Authorize (Roles= "Employees")]
         public ActionResult Index()
         {
+            //Users will see a list of all orders
+            return View();
+        }
+
+        // GET: Order/Index/5
+        [Authorize(Roles = "Contractors, Customers")]
+        public ActionResult Index(int id)
+        {
+            //customer or contractor will see a list of past and present orders associated to them
             return View();
         }
 
         // GET: Order/Details/5
+        [Authorize (Roles = "Employees, Contractors, Customers")]
         public ActionResult Details(int id)
         {
+            //Will get detailed information on a specific order
             return View();
         }
 
         // GET: Order/Create
+        [Authorize (Roles = "Customer, Employees")]
         public ActionResult Create()
         {
+            //takes user to a form to create a new order
             return View();
         }
 
         // POST: Order/Create
         [HttpPost]
+        [Authorize(Roles = "Customer, Employees")]
         public ActionResult Create(FormCollection collection)
         {
+            //Takes the info from customer or employee and updates the database
             try
             {
                 // TODO: Add insert logic here
@@ -43,15 +55,19 @@ namespace PracticalWerewolf.Controllers
         }
 
         // GET: Order/Edit/5
+        [Authorize(Roles = "Customer, Employees")]
         public ActionResult Edit(int id)
         {
+            //Allow for the information to be updated
             return View();
         }
 
         // POST: Order/Edit/5
         [HttpPost]
+        [Authorize(Roles = "Customer, Employees")]
         public ActionResult Edit(int id, FormCollection collection)
         {
+            //Save the updated information to the database
             try
             {
                 // TODO: Add update logic here
@@ -65,15 +81,19 @@ namespace PracticalWerewolf.Controllers
         }
 
         // GET: Order/Delete/5
+        [Authorize(Roles = "Customer, Employees")]
         public ActionResult Delete(int id)
         {
+            //Gives customer and employee the option to delete an order
             return View();
         }
 
         // POST: Order/Delete/5
         [HttpPost]
+        [Authorize(Roles = "Customer, Employees")]
         public ActionResult Delete(int id, FormCollection collection)
         {
+            //updates the database by removing the specific order
             try
             {
                 // TODO: Add delete logic here
@@ -86,22 +106,38 @@ namespace PracticalWerewolf.Controllers
             }
         }
 
-        public ActionResult ContractorOrders(int id)
-        {
-            //Details on all the shipments for a contractor
-            return View();
-        }
-
-        public ActionResult RejectOrder(int id)
+        // POST: Order/Reject/5
+        [HttpPost]
+        [Authorize(Roles = "Contractor")]
+        public ActionResult Reject(int id)
         {
             //contractor has rejected offer and now we must find a new persona
             return View();
         }
 
-        public ActionResult UpdateOrder(int id)
+        // GET: Order/Confirmation/5
+        [Authorize(Roles = "Contractor")]
+        public ActionResult Confirmation(int id)
         {
-            //contractor sends in the order deliever confirmation or updates status
+            //Page for the contractor to collect signature once product is delivered
             return View();
+        }
+
+        //POST: Order/Confirmation/5
+        [Authorize(Roles = "Contractor")]
+        public ActionResult Confirmation(int id, FormCollection collection)
+        {
+            //updates the database by marking the order as completed
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
         }
     }
 }
--- PracticalWerewolf/Controllers/TruckController.cs
+++ PracticalWerewolf/Controllers/TruckController.cs
@@ -1,35 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace PracticalWerewolf.Controllers
 {
+
     public class TruckController : Controller
     {
         // GET: Truck
+        [Authorize(Roles = "Employees")]
         public ActionResult Index()
         {
+            //shows list of all truck options
             return View();
         }
 
         // GET: Truck/Details/5
+        [Authorize(Roles = "Contractor, Employees")]
         public ActionResult Details(int id)
         {
+            //Gets details on a specific truck
             return View();
         }
 
         // GET: Truck/Create
+        [Authorize(Roles = "Contractor")]
         public ActionResult Create()
         {
+            //Takes contractors to a form to add a truck to their account
             return View();
         }
 
         // POST: Truck/Create
         [HttpPost]
+        [Authorize(Roles = "Contractor")]
         public ActionResult Create(FormCollection collection)
         {
+            //Updates the database with the new truck
             try
             {
                 // TODO: Add insert logic here
@@ -43,40 +48,22 @@ namespace PracticalWerewolf.Controllers
         }
 
         // GET: Truck/Edit/5
+        [Authorize(Roles = "Contractor")]
         public ActionResult Edit(int id)
         {
+            //User is taken to a page where they can change information
             return View();
         }
 
         // POST: Truck/Edit/5
         [HttpPost]
+        [Authorize(Roles = "Contractor")]
         public ActionResult Edit(int id, FormCollection collection)
         {
+            //The database is updated with the new user information
             try
             {
                 // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Truck/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Truck/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
 
                 return RedirectToAction("Index");
             }
