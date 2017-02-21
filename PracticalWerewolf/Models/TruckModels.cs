efb9f43fb5798ef2dbad5a3ac24356901ab3297f
James Onnen <rbxquenty@gmail.com>
2017-02-21 13:13:29 -0600

    Add truck models #15

---
+++ PracticalWerewolf/Models/TruckModels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Device.Location;
+using System.Linq;
+using System.Web;
+
+namespace PracticalWerewolf.Models
+{
+    public class Truck : ITruck
+    {
+        public ITruckCapacityUnit AvailableCapacity { get; set; }
+        public ContractorInfo Contractor { get; set; }
+        public ITruckCapacityUnit CurrentCapacity { get; set; }
+        public GeoCoordinate Location { get; set; }
+        public ITruckCapacityUnit MaxCapacity { get; set; }
+    }
+}
