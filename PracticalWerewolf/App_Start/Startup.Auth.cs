18491c448ed984cea669b21367a7811ba2858846
ajarratt06 <ajarratt@cse.unl.edu>
2017-02-24 15:40:18 -0600

    4 add controllers (#20)
    * Controllers and some cool stuff
    
    * Added controllers and actions #4
    
    * Updated controllers
    
    Controllers have roles. Saving now so I can roll back if the migration
    goes terribly
    
    * Fixed comments because they made me sad
    
    * Moved files to a viewmodel folder
    
    * fixed comments and reviewed names
    
    * Fixed some copy paste errors
    
    * refactored... deleteed usermanager
    
    * Moved files

--- PracticalWerewolf/App_Start/Startup.Auth.cs
+++ PracticalWerewolf/App_Start/Startup.Auth.cs
@@ -19,6 +19,7 @@ namespace PracticalWerewolf
             app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
             app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
 
+
             // Enable the application to use a cookie to store information for the signed in user
             // and to use a cookie to temporarily store information about a user logging in with a third party login provider
             // Configure the sign in cookie
