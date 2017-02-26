1ca37636fac44883aa5f0b78144a1adad06dd05c
ajarratt06 <ajarratt@cse.unl.edu>
2017-02-26 17:35:15 -0600

    29 fixing namespace on really old branch (#35)
    * Controllers and some cool stuff
    
    * Switched viewmodel namespace
    
    * Update OrderController.cs
    
    * Update OrderController.cs
    
    * Update TruckController.cs

--- PracticalWerewolf/ViewModels/ManageViewModels.cs
+++ PracticalWerewolf/ViewModels/ManageViewModels.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNet.Identity;
 using Microsoft.Owin.Security;
 
-namespace PracticalWerewolf.Models
+namespace PracticalWerewolf.ViewModels
 {
     public class IndexViewModel
     {
