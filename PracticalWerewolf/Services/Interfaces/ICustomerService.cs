a21c56ea194a40be2414492fc77bfadf3ddd49cf
mattdott123 <matthewdottavio@gmail.com>
2017-02-26 18:53:59 -0600

    19 Interface methods and Migration Fix (#43)
    * Added methods to Interfaces
    
    * Fix issues with integration
    
    * Fix naming on a few methods
    
    * Added parameters
    
    * Fixes casing in parameter
    
    * Fixes parameter Casing in Service
    
    * Update TruckService parameter
    
    * Fix ITruckService method parameter
    
    * Changes Namespace for EmployeeStore
    
    * Fix Migration issues

---
+++ PracticalWerewolf/Services/Interfaces/ICustomerService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using PracticalWerewolf.Models;
+using PracticalWerewolf.Models.Orders;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticalWerewolf.Services.Interfaces
+{
+    interface ICustomerService
+    {
+
+        //Create Order
+        void CreateOrderRequest(OrderRequestInfo orderRequestInfo);
+
+        //See current orders
+        IEnumerable<Order> ViewCurrentOrders(Guid customerInfoId);
+
+        //See order history
+        IEnumerable<Order> ViewPreviousOrders(Guid customerInfoId);
+
+        //Change billing info
+        //void UpdateBillingInfo(BillingInfo billingInfo);
+        //TODO?
+
+        //Cancel order
+        void DeleteOrder(Guid orderGuid);
+
+        //See billing info
+        //IBillingInfo GetBillingInfo();
+
+        //Change order
+        void ChangeOrder(Order order);
+
+        //Cancel order
+        void CancelOrder(Guid orderGuid);
+    }
+}
--- PracticalWerewolf/Services/OrderService.cs
+++ PracticalWerewolf/Services/OrderService.cs
@@ -4,16 +4,38 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using PracticalWerewolf.Models;
+using PracticalWerewolf.Models.Orders;
 
 namespace PracticalWerewolf.Services
 {
-    public class OrderService : IOrderManager
+    public class OrderService : IOrderService
     {
         private IOrderStore OrderStore;
 
         public OrderService(IOrderStore store)
         {
             OrderStore = store;
+        }
+
+        public void CreateOrder(Order order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteOrder(Guid orderGuid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Order GetOrder(Guid orderGuid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateOrder(Order order)
+        {
+            throw new NotImplementedException();
         }
     }
 }
