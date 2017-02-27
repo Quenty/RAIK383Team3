77b79dce7006d0295055fe51dcb07a8d2b021256
jesseez <jesseelzhang@gmail.com>
2017-02-26 18:17:18 -0600

    Revert "#19 Added methods to Interfaces" (#40)

--- PracticalWerewolf/Services/ContractorService.cs
+++ PracticalWerewolf/Services/ContractorService.cs
@@ -4,9 +4,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using PracticalWerewolf.Models;
-using PracticalWerewolf.Models.Orders;
-using PracticalWerewolf.Models.UserInfos;
 
 namespace PracticalWerewolf.Services
 {
@@ -17,36 +14,6 @@ namespace PracticalWerewolf.Services
         public ContractorService(IContractorStore store)
         {
             ContractorStore = store;
-        }
-
-        public void ChangeContractorStatus(bool status)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<Order> GetCurrentOrders(Guid contractorGuid)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<Order> GetPreviousOrders(Guid contractorGuid)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void RejectOrder(Guid orderGuid)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void UpdateOrderTrackInfo(OrderTrackInfo orderTrackInfo)
-        {
-            throw new NotImplementedException();
-        }
-
-        public CustomerInfo ViewCustomerInfo(Guid customerInfoGuid)
-        {
-            throw new NotImplementedException();
         }
     }
 }
