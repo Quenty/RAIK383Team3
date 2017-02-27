bc439740a7ce896e645e651a441e17b8405ee155
mattdott123 <matthewdottavio@gmail.com>
2017-02-26 18:07:22 -0600

    #19 Added methods to Interfaces (#24)
    * Added methods to Interfaces
    
    * Fix issues with integration
    
    * Fix naming on a few methods
    
    * Added parameters
    
    * Fixes casing in parameter
    
    * Fixes parameter Casing in Service
    
    * Update TruckService parameter
    
    * Fix ITruckService method parameter

--- PracticalWerewolf/Services/CustomerService.cs
+++ PracticalWerewolf/Services/CustomerService.cs
@@ -4,16 +4,48 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using PracticalWerewolf.Models;
+using PracticalWerewolf.Models.Orders;
 
 namespace PracticalWerewolf.Services
 {
-    public class CustomerService : ICustomerManager
+    public class CustomerService : ICustomerService
     {
         private ICustomerStore CustomerStore;
 
         public CustomerService(ICustomerStore store)
         {
             CustomerStore = store;
+        }
+
+        public void CancelOrder(Guid orderGuid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ChangeOrder(Order order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateOrderRequest(OrderRequestInfo orderRequestInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteOrder(Guid orderGuid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Order> ViewCurrentOrders(Guid customerInfoId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Order> ViewPreviousOrders(Guid customerInfoId)
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- PracticalWerewolf/Services/EmployeeService.cs
+++ PracticalWerewolf/Services/EmployeeService.cs
@@ -4,16 +4,70 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using PracticalWerewolf.Models;
+using PracticalWerewolf.Models.Orders;
+using PracticalWerewolf.Models.Trucks;
+using PracticalWerewolf.Models.UserInfos;
 
 namespace PracticalWerewolf.Services
 {
-    public class EmployeeService : IEmployeeManager
+    public class EmployeeService : IEmployeeService
     {
         private IEmployeeStore EmployeeStore;
 
         public EmployeeService(IEmployeeStore store)
         {
             EmployeeStore = store;
+        }
+
+        public void ApproveContractor(Guid contractorInfoGuid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Order> SearchOrders(string query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<UserInfo> SearchUsers(string query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateContractorInfo(ContractorInfo contractorInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCustomerInfo(CustomerInfo customerInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateEmployeeInfo(EmployeeInfo employeeInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateOrderInfo(Order order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateTruckInfo(Truck truck)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Order> ViewAllOrders()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<UserInfo> ViewUsers()
+        {
+            throw new NotImplementedException();
         }
     }
 }
