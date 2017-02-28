735f5da4ed5fb9c3376996fe86915bc0c4016970
jesseez <jesseelzhang@gmail.com>
2017-02-28 14:41:38 -0600

    #30 Sets up Dependency Injection (#53)
    * Adds implementations for service interfaces, changes IUserInfoContext to IUserInfoDbContext, Sets up Dependency Injection Registration
    
    * Update EmployeeService.cs

---
+++ PracticalWerewolf/Services/OrderTrackService.cs
@@ -0,0 +1,39 @@
+﻿using PracticalWerewolf.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PracticalWerewolf.Models.Orders;
+using PracticalWerewolf.Models.Trucks;
+using PracticalWerewolf.Models.UserInfos;
+
+namespace PracticalWerewolf.Services
+{
+    public class OrderTrackService : IOrderTrackService
+    {
+        public IEnumerable<Order> GetContractorOrders(Guid contractorInfoGuid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RejectOrder(Guid orderGuid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateOrderAssignee(Guid orderGuid, CustomerInfo customerInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateOrderStatus(Guid orderGuid, OrderStatus orderStatus)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateOrderTruck(Guid orderGuid, Truck currentTruck)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
