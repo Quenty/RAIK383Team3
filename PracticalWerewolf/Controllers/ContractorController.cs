29b9e7eb4bbe0a4d60030960ba63275c8bba4e01
James Onnen <rbxquenty@gmail.com>
2017-03-07 13:48:14 -0600

    Add some controller stuff

---
+++ PracticalWerewolf/Controllers/ContractorController.cs
@@ -0,0 +1,51 @@
+﻿using PracticalWerewolf.Models.UserInfos;
+using PracticalWerewolf.ViewModels.Contractor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PracticalWerewolf.Controllers
+{
+    [Authorize]
+    public class ContractorController : Controller
+    {
+        public enum ContractorMessageId
+        {
+            RegisterSuccess,
+            Error
+        }
+
+        public ActionResult Index(ContractorMessageId? message)
+        {
+            ViewBag.StatusMessage = 
+                message == ContractorMessageId.RegisterSuccess ? "Registered as a contractor succesfully"
+                : message == ContractorMessageId.Error ? "Error occured"
+                : "";
+
+
+            var model = new ContractorIndexModel
+            {
+                ContractorInfo = null,
+            };
+
+            return View(model);
+        }
+
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Register(ContractorRegisterModel model)
+        {
+
+            return View();
+        }
+    }
+}
---
+++ PracticalWerewolf/ViewModels/ContractorViewModels.cs
@@ -0,0 +1,20 @@
+﻿using PracticalWerewolf.Models.UserInfos;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Spatial;
+using System.Linq;
+using System.Web;
+
+namespace PracticalWerewolf.ViewModels.Contractor
+{
+    public class ContractorRegisterModel
+    {
+        
+    }
+
+    public class ContractorIndexModel
+    {
+        public bool HasContractorInfo { get; set;}
+        public ContractorInfo ContractorInfo { get; set; }
+    }
+}
