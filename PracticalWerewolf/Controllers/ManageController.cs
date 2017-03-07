108fdf42a9a0423f1d98b15bfbe8a206d92f9244
James Onnen <rbxquenty@gmail.com>
2017-03-07 13:48:06 -0600

    Remove bad items in the controller

--- PracticalWerewolf/Controllers/ManageController.cs
+++ PracticalWerewolf/Controllers/ManageController.cs
@@ -98,42 +98,6 @@ namespace PracticalWerewolf.Controllers
             return RedirectToAction("ManageLogins", new { Message = message });
         }
 
-
-        // GET: /Manage/Users
-        [Authorize (Roles = "Employees")]
-        public ActionResult Users()
-        {
-            //Get list of all users
-            return View();
-        }
-
-
-        public ActionResult ContractorShipments(int id)
-        {
-            //Details on all the shipments for a contractor
-            return View();
-        }
-
-        public ActionResult RejectOffer(int id)
-        {
-            //contractor has rejected offer and now we must find a new persona
-            return View();
-        }
-
-        public ActionResult UpdateOrder(int id)
-        {
-            //contractor sends in the order deliever confirmation or updates status
-            return View();
-        }
-
-        // GET: /Manage/Users/5
-        [Authorize(Roles = "Employees")]
-        public ActionResult Users(int id)
-        {
-            //Look at details for a specific user
-            return View();
-        }
-
         // GET: /Manage/AddPhoneNumber
         public ActionResult AddPhoneNumber()
         {
