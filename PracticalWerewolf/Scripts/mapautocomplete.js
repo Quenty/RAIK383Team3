fa52668a583e294789e21a359340277754798123
James Onnen <rbxquenty@gmail.com>
2017-03-15 20:20:52 -0500

    Solve issue when Google API fails.

--- PracticalWerewolf/Scripts/mapautocomplete.js
+++ PracticalWerewolf/Scripts/mapautocomplete.js
@@ -38,19 +38,28 @@ function initAutocomplete() {
 function fillInAddress() {
     // Get the place details from the autocomplete object.
     var place = autocomplete.getPlace();
+    if (place)
+    {
+        for (var component in componentForm) {
+            document.getElementById(component).value = '';
+            document.getElementById(component).disabled = false;
+        }
 
-    for (var component in componentForm) {
-        document.getElementById(component).value = '';
-        document.getElementById(component).disabled = false;
+        // Get each component of the address from the place details
+        // and fill the corresponding field on the form.
+        for (var i = 0; i < place.address_components.length; i++) {
+            var addressType = place.address_components[i].types[0];
+            if (componentForm[addressType]) {
+                var val = place.address_components[i][componentForm[addressType]];
+                document.getElementById(addressType).value = val;
+            }
+        }
     }
-
-    // Get each component of the address from the place details
-    // and fill the corresponding field on the form.
-    for (var i = 0; i < place.address_components.length; i++) {
-        var addressType = place.address_components[i].types[0];
-        if (componentForm[addressType]) {
-            var val = place.address_components[i][componentForm[addressType]];
-            document.getElementById(addressType).value = val;
+    else
+    {
+        console.write("No place loaded from autocomplete! Ran out of tokens?")
+        for (var component in componentForm) {
+            document.getElementById(component).disabled = false;
         }
     }
 }
