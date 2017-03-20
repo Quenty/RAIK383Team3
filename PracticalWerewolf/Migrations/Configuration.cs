63ff7d6a705310da37f1eac472be26ac5942c3ed
mattdott123 <matthewdottavio@gmail.com>
2017-03-20 01:47:38 -0500

    Change seeds to reflect refactoring

--- PracticalWerewolf/Migrations/Configuration.cs
+++ PracticalWerewolf/Migrations/Configuration.cs
@@ -45,9 +45,14 @@ namespace PracticalWerewolf.Migrations
                     );
             }
 
-            UserInfo user = new UserInfo { UserInfoGuid = Guid.NewGuid(), FirstName = "Bruce", LastName = "Wayne" };
+            UserInfo employeeUser = new UserInfo { UserInfoGuid = Guid.NewGuid(), FirstName = "Bruce", LastName = "Wayne" };
+            UserInfo contractorUser = new UserInfo { UserInfoGuid = Guid.NewGuid(), FirstName = "Eample", LastName = "Two" };
+            UserInfo customerUser = new UserInfo { UserInfoGuid = Guid.NewGuid(), FirstName = "Person", LastName = "Three" };
+            
             context.UserInfo.AddOrUpdate(
-                user
+                employeeUser,
+                contractorUser,
+                customerUser
                 );
 
             EmployeeInfo employee = new EmployeeInfo { EmployeeInfoGuid = Guid.NewGuid() };
@@ -60,43 +65,59 @@ namespace PracticalWerewolf.Migrations
                 customer
                 );
 
-            TruckCapacityUnit truckCapacityUnit = new TruckCapacityUnit { TruckCapacityUnitGuid = Guid.NewGuid() };
-            TruckCapacityUnit truckCapacityUnit2 = new TruckCapacityUnit { TruckCapacityUnitGuid = Guid.NewGuid() };
+            TruckCapacityUnit capacity = new TruckCapacityUnit { TruckCapacityUnitGuid = Guid.NewGuid() };
+            TruckCapacityUnit used = new TruckCapacityUnit { TruckCapacityUnitGuid = Guid.NewGuid() };
             context.TruckCapacityUnit.AddOrUpdate(
-                truckCapacityUnit,
-                truckCapacityUnit2
+                capacity,
+                used
             );
 
             DbGeography location = DbGeography.FromText("POINT(15 15)");
-            Truck truck = new Truck { TruckGuid = Guid.NewGuid(), LicenseNumber = "ABC123", Location = location, MaxCapacity = truckCapacityUnit, UsedCapacity = truckCapacityUnit2 };
+            Truck truck = new Truck { TruckGuid = Guid.NewGuid(), LicenseNumber = "ABC123", Location = location, MaxCapacity = capacity, UsedCapacity = used};
+            Truck truck2 = new Truck { TruckGuid = Guid.NewGuid(), LicenseNumber = "ABC123", Location = location, MaxCapacity = capacity, UsedCapacity = used };
+            Truck truck3 = new Truck { TruckGuid = Guid.NewGuid(), LicenseNumber = "ABC123", Location = location, MaxCapacity = capacity, UsedCapacity = used };
             context.Truck.AddOrUpdate(
-                truck
-                );
-
-
-
-            CivicAddressDb address = new CivicAddressDb { CivicAddressGuid = Guid.NewGuid(), StreetNumber = "640 N 14th St", Route = "Kauffman", City = "Lincoln", State = "NE", Country = "USA", RawInputAddress = "640 N 14h St. Lincoln NE 68508", ZipCode = "68508" };
-            ContractorInfo contractor = new ContractorInfo { ContractorInfoGuid = Guid.NewGuid(), ApprovalState = ContractorApprovalState.Approved, DriversLicenseId = "abc123", HomeAddress = address, IsAvailable = true, Truck = truck};
-
-            context.ContractorInfo.AddOrUpdate(
-                contractor
+                truck,
+                truck2,
+                truck3
             );
 
-            OrderRequestInfo orderRequest = new OrderRequestInfo { OrderRequestInfoGuid = Guid.NewGuid(), Requester = customer, Size = truckCapacityUnit, RequestDate = DateTime.Now, DropOffAddress = address, PickUpAddress = address };
+            CivicAddressDb address = new CivicAddressDb { CivicAddressGuid = Guid.NewGuid(), StreetNumber = "640 N 14th St", Route = "Kauffman", City = "Lincoln", State = "NE", Country = "USA", RawInputAddress = "640 N 14h St. Lincoln NE 68508", ZipCode = "68508" };
+            CivicAddressDb address2 = new CivicAddressDb { CivicAddressGuid = Guid.NewGuid(), StreetNumber = "641 N 14th St", Route = "Kauffman", City = "Lincoln", State = "NE", Country = "USA", RawInputAddress = "640 N 14h St. Lincoln NE 68508", ZipCode = "68508" };
+
+            ContractorInfo approvedContractor = new ContractorInfo { ContractorInfoGuid = Guid.NewGuid(), ApprovalState = ContractorApprovalState.Approved, DriversLicenseId = "abc123", HomeAddress = address, IsAvailable = true, Truck = truck};
+            ContractorInfo pendingContractor = new ContractorInfo { ContractorInfoGuid = Guid.NewGuid(), ApprovalState = ContractorApprovalState.Pending, DriversLicenseId = "abc1234", HomeAddress = address2, IsAvailable = false, Truck = truck2};
+            ContractorInfo deniedContractor = new ContractorInfo { ContractorInfoGuid = Guid.NewGuid(), ApprovalState = ContractorApprovalState.Denied, DriversLicenseId = "bc1234", HomeAddress = address2, IsAvailable = false, Truck = truck3};
+
+            context.ContractorInfo.AddOrUpdate(
+                approvedContractor,
+                pendingContractor,
+                deniedContractor
+            );
+
+            OrderRequestInfo orderRequest = new OrderRequestInfo { OrderRequestInfoGuid = Guid.NewGuid(), Requester = customer, Size = used, RequestDate = DateTime.Now, DropOffAddress = address, PickUpAddress = address2};
+            OrderRequestInfo orderRequest2 = new OrderRequestInfo { OrderRequestInfoGuid = Guid.NewGuid(), Requester = customer, Size = used, RequestDate = DateTime.Now, DropOffAddress = address, PickUpAddress = address2};
+            OrderRequestInfo orderRequest3 = new OrderRequestInfo { OrderRequestInfoGuid = Guid.NewGuid(), Requester = customer, Size = used, RequestDate = DateTime.Now, DropOffAddress = address, PickUpAddress = address2};
+            OrderRequestInfo orderRequest4 = new OrderRequestInfo { OrderRequestInfoGuid = Guid.NewGuid(), Requester = customer, Size = used, RequestDate = DateTime.Now, DropOffAddress = address, PickUpAddress = address2};
             context.OrderRequestInfo.AddOrUpdate(
                 orderRequest
                 );
 
-            OrderTrackInfo orderTrack = new OrderTrackInfo { OrderTrackInfoGuid = Guid.NewGuid(), Assignee = contractor, CurrentTruck = truck, OrderStatus = OrderStatus.InProgress };
+            OrderTrackInfo orderInProgress = new OrderTrackInfo { OrderTrackInfoGuid = Guid.NewGuid(), Assignee = approvedContractor, CurrentTruck = truck, OrderStatus = OrderStatus.InProgress};
+            OrderTrackInfo orderCancelled = new OrderTrackInfo { OrderTrackInfoGuid = Guid.NewGuid(), Assignee = approvedContractor, CurrentTruck = truck, OrderStatus = OrderStatus.Cancelled};
+            OrderTrackInfo orderComplete = new OrderTrackInfo { OrderTrackInfoGuid = Guid.NewGuid(), Assignee = approvedContractor, CurrentTruck = truck, OrderStatus = OrderStatus.Complete};
+            OrderTrackInfo orderQueued = new OrderTrackInfo { OrderTrackInfoGuid = Guid.NewGuid(), Assignee = approvedContractor, CurrentTruck = truck, OrderStatus = OrderStatus.Queued};
             context.OrderTrackInfo.AddOrUpdate(
-                    orderTrack
+                    orderInProgress,
+                    orderCancelled,
+                    orderComplete,
+                    orderQueued
                 );
 
-            Order order = new Order { OrderGuid = Guid.NewGuid(), RequestInfo = orderRequest, TrackInfo = orderTrack };
+            Order order = new Order { OrderGuid = Guid.NewGuid(), RequestInfo = orderRequest, TrackInfo = orderInProgress};
             context.Order.AddOrUpdate(
                 order
                 );
-
         }
     }
 }
