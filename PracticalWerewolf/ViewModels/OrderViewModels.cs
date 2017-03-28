cedba89271f66081b9c12036670bd41104d7bf90
James Onnen <rbxquenty@gmail.com>
2017-03-28 13:32:43 -0500

    Remove duplicates, add OrderViewModels

---
+++ PracticalWerewolf/ViewModels/OrderViewModels.cs
@@ -0,0 +1,25 @@
+﻿using PracticalWerewolf.Models.Trucks;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Device.Location;
+using System.Linq;
+using System.Web;
+
+namespace PracticalWerewolf.ViewModels.Orders
+{
+    public class CreateOrderRequestViewModel
+    {
+        [Required]
+        public Guid CustomomerInfoGuid { get; set; }
+
+        [Required]
+        public CivicAddressDb PickUpAddress { get; set; }
+
+        [Required]
+        public TruckCapacityUnit Size { get; set; }
+
+        [Required]
+        public CivicAddressDb DropOffAddress { get; set; }
+    }
+}
