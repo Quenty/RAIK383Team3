2b7a876709b4648407295ffb69d31eb7d3e86e3f
James Onnen <rbxquenty@gmail.com>
2017-04-02 18:11:05 -0500

    Comments were faulty, the relationship stated in them was wrong. Removed comments to keep confusion down in the future.

--- PracticalWerewolf/Models/Orders/Order.cs
+++ PracticalWerewolf/Models/Orders/Order.cs
@@ -9,13 +9,7 @@ namespace PracticalWerewolf.Models.Orders
     {
         [Key]
         public Guid OrderGuid { get; set; }
-
-        // zero-or-one-to-one relationship, keeps track of who placed the order and where it goes
-        [Required]
         public virtual OrderRequestInfo RequestInfo { get; set; }
-
-        // zero-or-one-to-one relationship, keeps track of where the order is
-        [Required]
         public virtual OrderTrackInfo TrackInfo { get; set; }
     }
 }
