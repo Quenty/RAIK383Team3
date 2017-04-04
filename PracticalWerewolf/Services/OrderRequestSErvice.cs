da186bae90e723182b3751a15ce6b48e16ba2c55
James Onnen <rbxquenty@gmail.com>
2017-04-04 09:47:11 -0500

    Implement OrderRequestService.CreateOrderRequest

--- PracticalWerewolf/Services/OrderRequestSErvice.cs
+++ PracticalWerewolf/Services/OrderRequestSErvice.cs
@@ -4,11 +4,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using PracticalWerewolf.Models.Orders;
+using PracticalWerewolf.Stores.Interfaces;
 
 namespace PracticalWerewolf.Services
 {
     public class OrderRequestService : IOrderRequestService
     {
+        private readonly IOrderStore OrderStore;
+
+        public OrderRequestService(IOrderStore OrderStore)
+        {
+            this.OrderStore = OrderStore;
+        }
+
         public IEnumerable<Order> GetCustomerOrders(Guid customerInfoGuid)
         {
             throw new NotImplementedException();
@@ -21,7 +29,20 @@ namespace PracticalWerewolf.Services
 
         public void CreateOrderRequestInfo(OrderRequestInfo requestInfo)
         {
-            throw new NotImplementedException();
+            if (requestInfo.OrderRequestInfoGuid == Guid.Empty)
+            {
+                throw new ArgumentException("Empty GUID in OrderRequestInfo");
+            }
+
+            OrderStore.Insert(new Order
+            {
+                OrderGuid = Guid.NewGuid(),
+                RequestInfo = requestInfo,
+                TrackInfo = new OrderTrackInfo
+                {
+                    OrderTrackInfoGuid = Guid.NewGuid()
+                }
+            });
         }
     }
 }
