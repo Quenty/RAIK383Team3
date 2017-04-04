0fb6f8c350118341dcd4031e6dd6dd5ff9f53a31
jesseez <jesseelzhang@gmail.com>
2017-04-04 13:09:19 -0500

    adds order service updateOrderStatus stuff

--- PracticalWerewolf/Services/OrderService.cs
+++ PracticalWerewolf/Services/OrderService.cs
@@ -6,16 +6,21 @@ using System.Web;
 using PracticalWerewolf.Models.Orders;
 using PracticalWerewolf.Models.UserInfos;
 using PracticalWerewolf.Stores.Interfaces;
+using System.Text;
+using log4net;
 
 namespace PracticalWerewolf.Services
 {
     public class OrderService : IOrderService
     {
+        private static ILog logger = LogManager.GetLogger(typeof(OrderService));
         private readonly IOrderStore _orderStore;
+        private readonly ApplicationUserManager _userManager;
 
-        public OrderService (IOrderStore OrderStore)
+        public OrderService (IOrderStore orderStore, ApplicationUserManager userManager)
         {
-            _orderStore = OrderStore;
+            _orderStore = orderStore;
+            _userManager = userManager;
         }
 
 
@@ -70,6 +75,54 @@ namespace PracticalWerewolf.Services
         {
             var allOrders = _orderStore.Find(o => o.TrackInfo.Assignee.ContractorInfoGuid == contractor.ContractorInfoGuid);
             return allOrders.Where(o => o.TrackInfo.OrderStatus == OrderStatus.Queued).ToList();
+        }
+
+        public async void SetOrderAsComplete(Guid orderId)
+        {
+            var order = _orderStore.Find(orderId);
+            if (order != null)
+            {
+                order.TrackInfo.OrderStatus = OrderStatus.Complete;
+                _orderStore.Update(order);
+
+                var customerId = order.RequestInfo.Requester.CustomerInfoGuid;
+                var customer = _userManager.Users.Single(x => x.CustomerInfo.CustomerInfoGuid == customerId);
+
+                StringBuilder emailBody = new StringBuilder();
+                emailBody.Append($"Your order {order.OrderGuid} has been delivered!\n");
+                emailBody.Append("Thank you for working with Practical Werewolf.\n");
+                emailBody.Append("If you have any questions, comments, or concerns, don't hesitate to contact us at PracticalWerewolf@gmail.com.");
+
+                await _userManager.SendEmailAsync(customer.Id, "Your order has been delivered!", emailBody.ToString());
+            }
+            else
+            {
+                logger.Error($"SetOrderAsComplete() - No order with id {orderId.ToString()}");
+            }
+        }
+
+        public async void SetOrderAsInProgress(Guid orderId)
+        {
+            var order = _orderStore.Find(orderId);
+            if (order != null)
+            {
+                order.TrackInfo.OrderStatus = OrderStatus.InProgress;
+                _orderStore.Update(order);
+
+                var customerId = order.RequestInfo.Requester.CustomerInfoGuid;
+                var customer = _userManager.Users.Single(x => x.CustomerInfo.CustomerInfoGuid == customerId);
+
+                StringBuilder emailBody = new StringBuilder();
+                emailBody.Append($"Your order {order.OrderGuid} has shipped!\n");
+                emailBody.Append("Thank you for working with Practical Werewolf.\n");
+                emailBody.Append("If you have any questions, comments, or concerns, don't hesitate to contact us at PracticalWerewolf@gmail.com.");
+
+                await _userManager.SendEmailAsync(customer.Id, "Your order has shipped!", emailBody.ToString());
+            }
+            else
+            {
+                logger.Error($"SetOrderAsInProgress() - No order with id {orderId.ToString()}");
+            }
         }
     }
 }
