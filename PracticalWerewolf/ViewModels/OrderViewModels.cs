920316b2652c96ea375f71c34cee27370a209e6f
James Onnen <rbxquenty@gmail.com>
2017-04-04 10:16:42 -0500

    Update display names on OrderViewModels

--- PracticalWerewolf/ViewModels/OrderViewModels.cs
+++ PracticalWerewolf/ViewModels/OrderViewModels.cs
@@ -11,12 +11,15 @@ namespace PracticalWerewolf.ViewModels.Orders
     public class CreateOrderRequestViewModel
     {
         [Required]
+        [Display(Name = "Pick up address", Description = "Where to pick up your package")]
         public CivicAddressDb PickUpAddress { get; set; }
 
         [Required]
+        [Display(Name = "Package size", Description = "Approximately how big is your package")]
         public TruckCapacityUnit Size { get; set; }
 
         [Required]
+        [Display(Name = "Drop off address", Description = "Where your package going")]
         public CivicAddressDb DropOffAddress { get; set; }
     }
 }
