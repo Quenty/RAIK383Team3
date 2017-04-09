c4998525f814ea0845a7844b2735d982311536f7
jesseez <jesseelzhang@gmail.com>
2017-04-09 16:18:24 -0500

    adds GoogleMapsApi and locationHelper methods

--- PracticalWerewolf/Controllers/LocationHelper.cs
+++ PracticalWerewolf/Controllers/LocationHelper.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using GoogleMapsApi;
+using GoogleMapsApi.Entities.Directions.Request;
+using GoogleMapsApi.Entities.Directions.Response;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity.Spatial;
+using System.Device.Location;
 using System.Linq;
 using System.Web;
 
@@ -13,6 +17,24 @@ namespace PracticalWerewolf.Controllers
             string wkt = String.Format("POINT({0} {1})", lon, lat);
 
             return DbGeography.PointFromText(wkt, srid);
+        }
+
+        public static DirectionsResponse GetRouteBetweenLocations(string origin, string destination)
+        {
+            DirectionsRequest directionsRequest = new DirectionsRequest()
+            {
+                Origin = origin,
+                Destination = destination
+            };
+
+            //There are many errors that will need to be handled higher in the call stack
+            //Checkt them with DirectionResponse.Status
+            return GoogleMaps.Directions.Query(directionsRequest);
+        }
+
+        public static DirectionsResponse GetRouteBetweenLocations(CivicAddressDb origin, CivicAddressDb destination)
+        {
+            return GetRouteBetweenLocations(origin, destination);
         }
     }
 }
