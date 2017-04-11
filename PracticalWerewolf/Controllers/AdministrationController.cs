a0790ceffb15e0bb2aeb189de0f94b3ca25cb9d0
James Onnen <rbxquenty@gmail.com>
2017-04-11 12:41:00 -0500

    Base administration commands

---
+++ PracticalWerewolf/Controllers/AdministrationController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PracticalWerewolf.Controllers
+{
+    [Authorize(Roles = "Employee")]
+    public class AdministrationController : Controller
+    {
+        private readonly ApplicationUserManager UserManager;
+
+        public AdministrationController(ApplicationUserManager UserManager)
+        {
+            this.UserManager = UserManager;
+        }
+
+        
+        public async Task<ActionResult> BanUser(string UserId)
+        {
+            if (UserId == null)
+            {
+                // TODO: error
+            }
+
+            await UserManager.SetLockoutEnabledAsync(UserId, true);
+            await UserManager.SetLockoutEndDateAsync(UserId, DateTime.Today.AddYears(10));
+
+            return Redirect(Request.UrlReferrer.ToString());
+        }
+
+        
+        public ActionResult SetEmployee(string UserId)
+        {
+            var User = UserManager.FindByIdAsync(UserId).Result;
+
+            if (User.EmployeeInfo != null)
+            {
+                // TODO: Error
+            }
+
+            User.EmployeeInfo = new Models.UserInfos.EmployeeInfo
+            {
+                EmployeeInfoGuid = Guid.NewGuid()
+            };
+
+            var result = UserManager.UpdateAsync(User).Result;
+
+            // TODO: Check result
+
+            return Redirect(Request.UrlReferrer.ToString());
+        }
+    }
+}
