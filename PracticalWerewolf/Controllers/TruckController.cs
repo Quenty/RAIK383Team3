998f02c1216d799b0a6f221277053c0405c3a0ec
James Onnen <rbxquenty@gmail.com>
2017-04-18 15:48:31 -0500

    Redirect back to the right tab

--- PracticalWerewolf/Controllers/TruckController.cs
+++ PracticalWerewolf/Controllers/TruckController.cs
@@ -247,12 +247,12 @@ namespace PracticalWerewolf.Controllers
 
                     TruckService.UpdateTruckLocation(returnedModel.Guid, location);
                     UnitOfWork.SaveChanges();
-                    return Redirect(Url.Action("Index", "Contractor", new { Message = ContractorController.ContractorMessageId.TruckLocationUpdatedSuccess }) + "truck");
+                    return Redirect(Url.Action("Index", "Contractor", new { Message = ContractorController.ContractorMessageId.TruckLocationUpdatedSuccess }) + "#truck");
                 }
                 catch
                 {
                     logger.Error("UpdateLocation - Error in UpdateTruckLocation");
-                    return Redirect(Url.Action("Index", "Contractor", new { Message = ContractorController.ContractorMessageId.TruckLocationUpdateError }) + "truck");
+                    return Redirect(Url.Action("Index", "Contractor", new { Message = ContractorController.ContractorMessageId.TruckLocationUpdateError }) + "#truck");
                 }
             }
             else
